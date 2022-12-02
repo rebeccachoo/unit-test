c0312b2b5b71ee68ddd911398ee8f50503eff8af
Rebecca Choo <rebeccachoo@Rebeccas-Laptop.local>
2022-12-02 00:01:17 -0600

    Class Added

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -2,4 +2,5 @@ const add = require("../add");
 
 test("", () => {
   // code
+  expect(add(1, 3)).toBe(4);
 });
---
+++ src/test/calculator.test.js
@@ -0,0 +1,7 @@
+const calculator = require("../calculator");
+
+// describe은 관련있는 테스트를 그룹으로 만드는것
+describe("Calculator", () => {
+  // it 은 calculator를 가르킴 test라고 해도됨
+  it("init with 0", () => {});
+});
