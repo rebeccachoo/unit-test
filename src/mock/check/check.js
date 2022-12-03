7a87fa9b27fa6fc4b4bb82a1a01ed514001c1cda
Rebecca <rnwldms@gmail.com>
2022-12-02 23:45:03 -0600

    Mock Functions Added

--- src/mock/check/check.js
+++ src/mock/check/check.js
@@ -5,4 +5,5 @@ function check(predicate, onSuccess, onFail) {
     onFail("no");
   }
 }
+
 module.exports = check;
--- src/mock/check/test/check.test.js
+++ src/mock/check/test/check.test.js
@@ -3,9 +3,16 @@ const check = require("../check");
 describe("check", () => {
   let onSuccess;
   let onFail;
-
   beforeEach(() => {
     onSuccess = jest.fn();
     onFail = jest.fn();
+  });
+  it("should call onSuccess when predicate is true", () => {
+    check(() => true, onSuccess, onFail);
+    expect(onSuccess.mock.calls.length).toBe(1); // onSuccess가 한번 불렸는지 테스트
+    // expect(onSuccess.mock.calls[0][0]).toBe("yes"); // onSuccess가 한번 불렸는지 테스트
+    expect(onSuccess).toHaveBeenCalledWith("yes");
+    expect(onFail.mock.calls.length).toBe(0); // onFail 한번 불렸는지 테스트
+    expect(onFail).toHaveBeenCalledTimes(0);
   });
 });
